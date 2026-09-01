c83dbdaa0ea5ccc2c1bb506c3ac8e5be1883263d
alkavan <igal@alkontek.com>
2026-09-01 14:11:37 +0300

    use 512-bit registers for F64 AVX-512 mat4 mul/transpose.

--- src/simd/avx512/matrix4_ptr.c
+++ src/simd/avx512/matrix4_ptr.c
@@ -8,48 +8,53 @@
 
 #if defined(VECMAT_USE_F64)
 
+/* One f64 column is 256 bits. Duplicate it into both halves of a ZMM so two
+ * result columns can FMA in one instruction. */
+static inline __m512d vm_avx512_dupcol(const __m256d col)
+{
+    return _mm512_insertf64x4(_mm512_castpd256_pd512(col), col, 1);
+}
+
+static inline __m512d vm_avx512_dup2(const double s0, const double s1)
+{
+    return _mm512_setr_pd(s0, s0, s0, s0, s1, s1, s1, s1);
+}
+
 void mat4_mul_ptr_avx512(matrix4 *res, const matrix4 *a, const matrix4 *b)
 {
     matrix4 tmp;
-    const __m256d a0 = _mm256_loadu_pd(&a->v[0]);
-    const __m256d a1 = _mm256_loadu_pd(&a->v[4]);
-    const __m256d a2 = _mm256_loadu_pd(&a->v[8]);
-    const __m256d a3 = _mm256_loadu_pd(&a->v[12]);
-    for (int c = 0; c < 4; c++) {
-        __m256d col = _mm256_mul_pd(a0, _mm256_set1_pd(b->v[c * 4 + 0]));
-        col = _mm256_fmadd_pd(a1, _mm256_set1_pd(b->v[c * 4 + 1]), col);
-        col = _mm256_fmadd_pd(a2, _mm256_set1_pd(b->v[c * 4 + 2]), col);
-        col = _mm256_fmadd_pd(a3, _mm256_set1_pd(b->v[c * 4 + 3]), col);
-        _mm256_storeu_pd(&tmp.v[c * 4], col);
+    const __m512d a0 = vm_avx512_dupcol(_mm256_loadu_pd(&a->v[0]));
+    const __m512d a1 = vm_avx512_dupcol(_mm256_loadu_pd(&a->v[4]));
+    const __m512d a2 = vm_avx512_dupcol(_mm256_loadu_pd(&a->v[8]));
+    const __m512d a3 = vm_avx512_dupcol(_mm256_loadu_pd(&a->v[12]));
+
+    /* C.col[j] and C.col[j+1] packed in one ZMM. */
+    for (int c = 0; c < 4; c += 2) {
+        __m512d acc = _mm512_mul_pd(a0, vm_avx512_dup2(b->v[c * 4 + 0], b->v[(c + 1) * 4 + 0]));
+        acc = _mm512_fmadd_pd(a1, vm_avx512_dup2(b->v[c * 4 + 1], b->v[(c + 1) * 4 + 1]), acc);
+        acc = _mm512_fmadd_pd(a2, vm_avx512_dup2(b->v[c * 4 + 2], b->v[(c + 1) * 4 + 2]), acc);
+        acc = _mm512_fmadd_pd(a3, vm_avx512_dup2(b->v[c * 4 + 3], b->v[(c + 1) * 4 + 3]), acc);
+        _mm512_storeu_pd(&tmp.v[c * 4], acc);
     }
     memcpy(res->v, tmp.v, sizeof(tmp.v));
 }
 
 void mat4_transpose_ptr_avx512(matrix4 *res, const matrix4 *m)
 {
-    __m256d r0 = _mm256_loadu_pd(&m->v[0]);
-    __m256d r1 = _mm256_loadu_pd(&m->v[4]);
-    __m256d r2 = _mm256_loadu_pd(&m->v[8]);
-    __m256d r3 = _mm256_loadu_pd(&m->v[12]);
-
-    __m256d t0 = _mm256_unpacklo_pd(r0, r1);
-    __m256d t1 = _mm256_unpackhi_pd(r0, r1);
-    __m256d t2 = _mm256_unpacklo_pd(r2, r3);
-    __m256d t3 = _mm256_unpackhi_pd(r2, r3);
-
-    r0 = _mm256_permute2f128_pd(t0, t2, 0x20);
-    r1 = _mm256_permute2f128_pd(t1, t3, 0x20);
-    r2 = _mm256_permute2f128_pd(t0, t2, 0x31);
-    r3 = _mm256_permute2f128_pd(t1, t3, 0x31);
-
+    /* Two ZMMs hold the whole 4×4. Index bit 3 picks lo vs hi. */
+    const __m512d lo = _mm512_loadu_pd(&m->v[0]);
+    const __m512d hi = _mm512_loadu_pd(&m->v[8]);
+    const __m512d out_lo = _mm512_permutex2var_pd(
+        lo, _mm512_setr_epi64(0, 4, 8, 12, 1, 5, 9, 13), hi);
+    const __m512d out_hi = _mm512_permutex2var_pd(
+        lo, _mm512_setr_epi64(2, 6, 10, 14, 3, 7, 11, 15), hi);
     matrix4 tmp;
-    _mm256_storeu_pd(&tmp.v[0], r0);
-    _mm256_storeu_pd(&tmp.v[4], r1);
-    _mm256_storeu_pd(&tmp.v[8], r2);
-    _mm256_storeu_pd(&tmp.v[12], r3);
+    _mm512_storeu_pd(&tmp.v[0], out_lo);
+    _mm512_storeu_pd(&tmp.v[8], out_hi);
     memcpy(res->v, tmp.v, sizeof(tmp.v));
 }
 
+/* 4 f64 lanes = one YMM; a ZMM would be half unused. */
 void mat4_mul_vec4_ptr_avx512(vector4 *res, const matrix4 *m, const vector4 *v)
 {
     __m256d r = _mm256_mul_pd(_mm256_loadu_pd(&m->v[0]), _mm256_set1_pd(v->x));
